90ac40edb871aea1bf104716fb370ba2cb4ce6a3
Mari Khachikyan <marikhachikyan077@gmail.com>
2022-06-09 22:42:56 +0400

    Add project files.

---
+++ ShopDatabase/StoredProcedures/UpdateUser.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[UpdateUser]
+	@Id int,
+	@Name nvarchar(20),
+	@Surname nvarchar(20),
+	@AccountNumber nvarchar(20),
+	@Balance SMALLMONEY,
+	@Picture VARBINARY(MAX),
+	@Address nvarchar(20),
+	@DateOfBirth datetime2,
+	@Login varchar(20),
+	@Password varchar(20)
+AS
+	UPDATE Users
+	SET Id = @Id, Name = @Name, 
+	where UserId = @UserId
+RETURN 0
