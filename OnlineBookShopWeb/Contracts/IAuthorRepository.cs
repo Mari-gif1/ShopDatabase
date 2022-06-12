b232f5d924a4b4ddd813a90411690067890ce685
Julieta Soghomonyan <julieta.soghomonyan@gmail.com>
2022-06-12 22:26:49 +0400

    Changes

---
+++ OnlineBookShopWeb/Contracts/IAuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnlineBookShopWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookShopWeb.Contracts
+{
+    public interface IAuthorRepository : IBaseRepository<Author>
+    {
+
+    }
+}
---
+++ OnlineBookShopWeb/Models/Author.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookShopWeb.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("First Name")]
+        public string Firstname { get; set; }
+        [Required]
+        [DisplayName("Last Name")]
+        public string Lastname { get; set; }
+
+        public virtual IList<Book> Books { get; set; }
+    }
+}
