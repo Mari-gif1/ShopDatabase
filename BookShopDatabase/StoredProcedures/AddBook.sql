9b8b2e6d29d5346e54fa175ccbe8391824c209cf
Julieta Soghomonyan <julieta.soghomonyan@gmail.com>
2022-06-13 21:43:22 +0400

    New changes

--- BookShopDatabase/StoredProcedures/AddBook.sql
+++ BookShopDatabase/StoredProcedures/AddBook.sql
@@ -3,8 +3,9 @@
 	@Cost money,
 	@Genre nvarchar(30),
 	@Author nvarchar(100),
-	@PublishYear int
+	@PublishYear int,
+	@imageUrl varchar(2048)
 AS
-	INSERT INTO [Books]([Title], [Cost], [Genre], [Author], [PublishYear])
-	VALUES(@Title, @Cost, @Genre, @Author, @PublishYear)
+	INSERT INTO [Books]([Title], [Cost], [Genre], [Author], [PublishYear],[imageUrl])
+	VALUES(@Title, @Cost, @Genre, @Author, @PublishYear, @imageUrl)
 RETURN SCOPE_IDENTITY()
