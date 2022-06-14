67ff0f2120ca7427fe2f7894a04374a0c0aef7c4
Julieta Soghomonyan <julieta.soghomonyan@gmail.com>
2022-06-14 04:49:15 +0400

    Blob Changes

--- OnlineBookShopWeb/Models/Book.cs
+++ OnlineBookShopWeb/Models/Book.cs
@@ -6,8 +6,8 @@ namespace OnlineBookShopWeb.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public int Year { get; set; }
-        public string Image { get; set; }
+        public int PublishYear { get; set; }
+        public string ImageURL { get; set; }
         public decimal Cost { get; set; }
         public string Author { get; set; }
         public string Genre { get; set; }
